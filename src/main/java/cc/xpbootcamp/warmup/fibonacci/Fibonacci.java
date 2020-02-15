e184f0bd288a985754723a50e2ef4b527775025b
facccge <fcfc5702@qq.com>
2020-02-15 18:04:03 +0800

    Fibonacci: Add logic of calculation using recursion.

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 public class Fibonacci {
     public long calculate(int position) {
-        if (position < 1 || position >50) {
+        if (position < 1 || position > 50) {
             return 0L;
         }
-        return 0L;
+        if (position == 1) {
+            return 1L;
+        }
+        return calculate(position - 1) + calculate(position - 2);
     }
 }
